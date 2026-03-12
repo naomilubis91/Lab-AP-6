c012259d6d0b0ec245e66b34f9eccaa4ce5e611a
naomilubis91 <naomilubis91@gmail.com>
2026-03-12 14:39:18 +0700

    Pertemuan 4 Lan AP 6

---
+++ Pertemuan 4/dowhile.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 11;
+
+    do {
+        cout << a << endl;
+        a++;
+    } while ( a <= 10 );
+}
---
+++ Pertemuan 4/for.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x;
+
+    cout << "Inputkan sebuah angka : ";
+    cin >> x;
+
+    // inisialisasi, kondisi, iterasi
+    for (int a = x; a <= 10; a++) {
+        cout << a << endl;
+    }
+
+    // proses 1 : a->1 lalu dicek kondisi benar -> tampilkan dulu baru ditambah
+}
